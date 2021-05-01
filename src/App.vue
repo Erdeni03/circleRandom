2e6a3ed020de25fe49fe0558412d273d3fb12a98
Erdeni <batorov-1995@mail.ru>
2021-05-01 22:56:51 +0300

    add code

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" @click="generate">
+      {{ circle.length ? "Сбросить" : "Сгенерировать" }}
+    </button>
+    <button type="button" @click="setSort">
+      Сортировка
+    </button>
+
+    <transition-group name="flip-list" tag="div" class="box">
+      <div
+        v-for="c in circle"
+        :key="c.color"
+        class="circle"
+        :style="{
+          backgroundColor: c.color
+        }"
+      ></div>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      circle: []
+    }
+  },
+
+  computed: {},
+  methods: {
+    setSort() {
+      this.circle
+        .sort((a, b) =>
+          this.optionSort(a.color) > this.optionSort(b.color) ? 1 : -1
+        )
+        .reverse()
+    },
+    optionSort(str) {
+      let rgb = str
+        .replace(/[rgb()]/g, "")
+        .split(",")
+        .map(Number)
+
+      return rgb[0] + rgb[1] + rgb[2]
+    },
+    getRandomRGB(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min
+    },
+    // getRandomPositionY() {
+    //   return Math.floor(Math.random() * 200)
+    // },
+    generate() {
+      const setLength = Math.floor(Math.random() * 10) + 1
+      if (!this.circle.length) {
+        for (let i = 0; i < setLength; i++) {
+          this.circle.push({
+            color: `rgb(${this.getRandomRGB(0, 255)},${this.getRandomRGB(
+              0,
+              255
+            )},${this.getRandomRGB(0, 255)})`
+            // position: this.getRandomPositionY() + "%"
+          })
+        }
+      } else {
+        this.circle = []
+      }
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: rgb(216, 216, 216);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +83,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.box {
+  display: flex;
+
+  justify-content: space-around;
+  align-items: center;
+
+  width: 800px;
+  height: 150px;
+  background-color: #fff;
+  margin: auto;
+  transform: translateY(50%);
+  position: relative;
+}
+.box .circle {
+  width: 50px;
+  height: 50px;
+  display: inline-block;
+  /* transform: translateY(200%) */
+  border-radius: 50%;
+}
+.flip-list-move {
+  transition: transform 1s;
 }
 </style>
